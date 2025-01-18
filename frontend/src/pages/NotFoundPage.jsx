3b3355326b4966ce5e4385b9ea3143f7f9d001a1
prathosh166@gmail.com <prathosh166@gmail.com>
2025-01-18 15:17:52 +0530

    Fix missing file issue-4

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,38 +1,10 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import Navbar from "../components/Navbar";
-// const NotFoundPage = () => {
-//   return (
-//     <div className=" bg-black min-h-screen text-white">
-//       <Navbar />
-//       <div
-//         className="h-[60vh] bg-cover bg-center flex flex-col justify-center items-center text-white"
-//         style={{ backgroundImage: `url('/404-error.jpg')` }}
-//       >
-       
-//       </div>
-//      <div className="text-center mt-8"> <Link to={"/"} className="bg-white btncolor text-white py-2 px-4 rounded ">
-//           Home
-//         </Link></div>
-//       <main className="text-center pb-14 pt-7 z-10">
-      
-//         <h1 className="text-7xl font-semibold mb-4">Lost your way?</h1>
-//         <p className=" text-xl">
-//           Sorry, we can't find that page. You'll find lots to explore on the
-//           home page.
-//         </p>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default NotFoundPage;
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { motion } from "framer-motion";
-
+import { useAuthStore } from "../store/authUser";
 const NotFoundPage = () => {
+  const {user } = useAuthStore();
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1, transition: { staggerChildren: 0.3 } },
@@ -44,10 +16,32 @@ const NotFoundPage = () => {
   };
 
   return (
-    <div className="bg-black pt-28 pb-10 min-h-screen text-white">
-      <Navbar />
+    <div className="bg-black pb-10 min-h-screen text-white">
+      {user ?( <div className="mt-32"><Navbar /></div> ) : (  <header className="max-w-8xl mx-auto flex items-center justify-between p-4">
+                <Link className="flex" to={"/"}>
+                  <img
+                    className="w-40 "
+                    src="/Flix-shadow.png"
+                    alt="Flix Maniac logo"
+                  />
+                </Link>
+                <div className="flex items-center space-x-4">
+                  <Link
+                    to={"/login"}
+                    className="text-white w-[75px] bg-[#3077a3] py-1 px-2 rounded"
+                  >
+                    Sign In
+                  </Link>
+                  <Link
+                    to={"/signup"}
+                    className="text-white w-[75px] bg-[#3077a3] py-1 px-2 rounded"
+                  >
+                    Sign Up
+                  </Link>{" "}
+                </div>
+              </header>)}
       <div
-        className="h-[60vh] md:object-contain object-cover w-full bg-cover bg-center flex flex-col justify-center items-center text-white"
+        className="h-[60vh]  md:object-contain object-cover w-full bg-cover bg-center flex flex-col justify-center items-center text-white"
         style={{ backgroundImage: `url('/404-error.jpg')` }}
       ></div>
       <motion.div
@@ -57,23 +51,20 @@ const NotFoundPage = () => {
         variants={containerVariants}
       >
         <motion.h1
-          className="text-7xl font-semibold mb-4"
+          className="md:text-7xl text-2xl font-semibold mb-4"
           variants={itemVariants}
         >
           Lost your way?
         </motion.h1>
-        <motion.p
-          className="text-xl mb-6"
-          variants={itemVariants}
-        >
+        <motion.p className="md:text-xl text-base mb-6" variants={itemVariants}>
           Sorry, we can't find that page. You'll find lots to explore on the
           home page.
         </motion.p>
-        <motion.div
-          className="mt-6"
-          variants={itemVariants}
-        >
-          <Link to={"/"} className="bg-white btncolor text-white py-2 px-4 rounded ">
+        <motion.div className="mt-6" variants={itemVariants}>
+          <Link
+            to={"/"}
+            className="bg-white btncolor text-white py-2 px-4 rounded "
+          >
             Home
           </Link>
         </motion.div>
