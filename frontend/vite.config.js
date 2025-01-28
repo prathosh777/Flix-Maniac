b5e28f3ba1f15f2462c20fa992633513ae5a0e51
prathosh166@gmail.com <prathosh166@gmail.com>
2025-01-28 23:31:40 +0530

    vite change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import {ENV_variables} from "../backend/config/envVariables.js"
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: ENV_variables.NODE_ENV === 'production' 
+        ? 'https://flix-maniac.vercel.app/'
+        : 'http://localhost:5000',
       },
     },
   },
